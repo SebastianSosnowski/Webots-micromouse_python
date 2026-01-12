23bdea245e1d3a7540ac89c6b3f208bef375ee1a
Sebastian Sosnowski <mefiu672@gmail.com>
2026-01-12 22:08:21 +0100

    Add config:
    Add config.yaml file with simulation, robot and maze parameters.
    Add config.py file with Enums to validate some config params.

---
+++ controllers/Maze_solver_py/config.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+
+class Mode(Enum):
+    """Run mode."""
+
+    SEARCH = 1
+    SPEEDRUN = 2
+
+
+class Algorithm(Enum):
+    """Algorithm to solve maze."""
+
+    KEYBOARD = 1
+    FLOODFILL = 2
+    DFS = 3
+    BFS = 4
+    A_STAR = 5
+    A_STAR_MOD = 6
+
+
+class MazeLayout(Enum):
+    """Maze layout corresponding to simulation wolrd."""
+
+    FORBOT = 1
+    TAIWAN_2015 = 2
+    APEC_2010 = 3
+    UK_2016 = 4
+    HIGASHI_2017 = 5
+    JAPAN_2013EQ = 6
+    KANKOU_2003 = 7
+    JAPAN_2011 = 8
+    JAPAN_1987 = 9
+    KOR_88 = 10
+
+
+class Directions(Enum):
+    """Wall value according to its global direction."""
+
+    WEST = 1  #  00000001
+    SOUTH = 2  #  00000010
+    EAST = 4  #  00000100
+    NORTH = 8  #  00001000
+    # NORTH = 1  #  00000001
+    # EAST = 2  #  00000010
+    # SOUTH = 4  #  00000100
+    # WEST = 8  #  00001000
