f8801853a3aed838e8e6713313a207b760482e90
Sebastian Sosnowski <mefiu672@gmail.com>
2026-02-08 20:48:44 +0100

    Cleanup

--- controllers/Maze_solver_py/main.py
+++ controllers/Maze_solver_py/main.py
@@ -1,90 +1,68 @@
-"""Maze Solver controller."""
-
+import sys
+from pathlib import Path
 from config.logging_config import setup_logging
 
 setup_logging()
 import logging
 
-logger = logging.getLogger(__name__)
-
-from pathlib import Path
-from queue import Queue
-from copy import deepcopy
+from PySide6.QtWidgets import QApplication, QGraphicsView
+from PySide6.QtCore import Qt
 
 from config.loader import load_config
-from config.enums import Mode
 from maze_solver import MazeSolver
-from draw.turtle_maze_drawer import MazeDrawer
-from utils.types import DrawState
-from read_files.storage import save_results
+from draw.solver_worker import SolverWorker
+from draw.qt_maze_drawer import MazeScene, MainWindow
+
+logger = logging.getLogger(__name__)
 
 
-def run_robot(mz: MazeSolver):
-    """Run the robot based on the configured algorithm.
+def main():
+    app = QApplication(sys.argv)
 
-    Args:
-        robot: The robot instance to control.
-    """
-    mz.algorithm.init()
-
-    path, maze_map, position_values = mz.init_drawer_values()
-
-    draw_queue = Queue(maxsize=1)
-    drawer = MazeDrawer(mz._cfg, deepcopy(maze_map), deepcopy(position_values), draw_queue)
-    drawer.start_drawing()
-    match mz._cfg.simulation.mode:
-        case Mode.SEARCH:
-            while mz.robot.robot.step(mz._cfg.simulation.time_step) != -1:
-                detected = mz.robot.read_sensors()
-                targets = mz.algorithm.update(detected, mz.robot.state)
-                while targets:
-                    draw_queue.put(
-                        DrawState(
-                            mz.robot.state.pos,
-                            deepcopy(mz.algorithm.maze_map),
-                            deepcopy(mz.algorithm.position_values),
-                        )
-                    )
-                    mz.robot.move(targets.pop(0))
-                if mz.algorithm.finish():
-                    draw_queue.put(
-                        DrawState(
-                            mz.robot.state.pos,
-                            deepcopy(mz.algorithm.maze_map),
-                            deepcopy(mz.algorithm.position_values),
-                        )
-                    )
-                    draw_queue.put(None)
-                    logger.info("Target reached")
-                    logger.info("Searching time: %.2f s", mz.robot.robot.getTime())
-                    path, maze_map, position_values = mz.algorithm.prepare_results()
-                    save_results(
-                        path,
-                        maze_map,
-                        position_values,
-                        mz._cfg.simulation.maze_layout,
-                        mz._cfg.simulation.algorithm,
-                    )
-                    break
-        case Mode.SPEEDRUN:
-            while mz.robot.robot.step(mz._cfg.simulation.time_step) != -1:
-                if not path:
-                    break
-                draw_queue.put(DrawState(mz.robot.state.pos, maze_map, position_values))
-                mz.robot.move(path.pop(0))
-            draw_queue.put(DrawState(mz.robot.state.pos, maze_map, position_values))
-            draw_queue.put(None)
-            logger.info("Target reached")
-            logger.info("Speedrun time: %.2f s", mz.robot.robot.getTime())
-
-    input("press any key to end")
-    exit(0)
-
-
-if __name__ == "__main__":
     config_path = Path("config.yaml")
-
     config = load_config(config_path)
     maze_solver = MazeSolver(config)
 
-    run_robot(maze_solver)
+    worker = SolverWorker(maze_solver)
+
+    # --- scene + view ---
+    scene = MazeScene(
+        config=config,
+        maze_map=maze_solver.algorithm.maze_map,
+        position_values=maze_solver.algorithm.position_values,
+        cell_size=60,
+    )
+
+    # --- view ---
+    view = QGraphicsView(scene)
+
+    view.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    view.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+    view.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+    # --- main window ---
+    window = MainWindow()
+    window.setWindowTitle("Micromouse Maze")
+    window.setCentralWidget(view)
+
+    scene_rect = scene.sceneRect()
+    EXTRA_MARGIN = 8
+
+    window.setFixedSize(
+        int(scene_rect.width()) + EXTRA_MARGIN,
+        int(scene_rect.height()) + EXTRA_MARGIN,
+    )
+
+    window.show()
+
+    # --- worker ---
+    worker = SolverWorker(maze_solver)
+    worker.draw_state.connect(scene.update_from_state)
+
+    worker.start()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
